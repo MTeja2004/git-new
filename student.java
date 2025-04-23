7f6cba8583eb5fde748168e1f07f53a4fd8fa5a9
root <root@ip-172-31-27-4.ec2.internal>
2025-04-23 19:20:28 +0000

    MC1

---
+++ student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public static void main(String[] args) {
+        int rollno = 1596;
+        String sname = "KUMAR";
+        String course = "PYTHON";
+        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
+        System.out.println("My Name is: " + sname);
+        System.out.println("My Course is: " + course);
+        System.out.println("My Roll Number is: " + rollno);
+        System.out.println("=======================");
+        System.out.println("GOOD MORNING");
+        System.out.println("THANK YOU");
+        System.out.println("THANK YOU");
+        System.out.println("THANK YOU");
+    }
+}
